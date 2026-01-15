527e575d2103335516b164f40b9eace2e968a8fc
KarimBassel <karimbassel15@gmail.com>
2026-01-15 21:52:17 +0200

    Updates Test Profile

--- booking/src/main/java/com/hotel/booking/config/AdminInitializer.java
+++ booking/src/main/java/com/hotel/booking/config/AdminInitializer.java
@@ -1,35 +1,35 @@
-package com.hotel.booking.config;
-
-import com.hotel.booking.model.Enums.Role;
-import com.hotel.booking.model.User;
-import com.hotel.booking.repository.UserRepository;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.annotation.Profile;
-import org.springframework.security.crypto.password.PasswordEncoder;
-
-@Profile("!test")
-@Configuration
-public class AdminInitializer {
-
-    @Bean
-    CommandLineRunner initAdmin(UserRepository userRepository,
-                                PasswordEncoder passwordEncoder) {
-
-        return args -> {
-            if (!userRepository.existsByEmail("admin@hotel.com")) {
-
-                User admin = new User();
-                admin.setName("System Admin");
-                admin.setEmail("admin@hotel.com");
-                admin.setPassword(passwordEncoder.encode("admin123"));
-                admin.setRole(Role.ADMIN);
-
-                userRepository.save(admin);
-
-                System.out.println("Default admin user created");
-            }
-        };
-    }
-}
+//package com.hotel.booking.config;
+//
+//import com.hotel.booking.model.Enums.Role;
+//import com.hotel.booking.model.User;
+//import com.hotel.booking.repository.UserRepository;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.boot.CommandLineRunner;
+//import org.springframework.context.annotation.Profile;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//
+//@Profile("!test")
+//@Configuration
+//public class AdminInitializer {
+//
+//    @Bean
+//    CommandLineRunner initAdmin(UserRepository userRepository,
+//                                PasswordEncoder passwordEncoder) {
+//
+//        return args -> {
+//            if (!userRepository.existsByEmail("admin@hotel.com")) {
+//
+//                User admin = new User();
+//                admin.setName("System Admin");
+//                admin.setEmail("admin@hotel.com");
+//                admin.setPassword(passwordEncoder.encode("admin123"));
+//                admin.setRole(Role.ADMIN);
+//
+//                userRepository.save(admin);
+//
+//                System.out.println("Default admin user created");
+//            }
+//        };
+//    }
+//}
