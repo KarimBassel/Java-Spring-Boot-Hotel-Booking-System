0fe09e45fa965e5361a83b2925cacfb2c8e38d22
KarimBassel <karimbassel15@gmail.com>
2026-06-26 18:23:00 +0300

    Exposing Version Endpoint

--- booking/src/main/java/com/hotel/booking/config/SecurityConfig.java
+++ booking/src/main/java/com/hotel/booking/config/SecurityConfig.java
@@ -51,6 +51,8 @@ public class SecurityConfig {
                                      "/swagger-ui.html",
                                      "/swagger-ui/**",
                                      "/v3/api-docs/**",
+                                     //For version checks on deployment
+                                     "/api/version",
                                      //Enable Access to stripe
                                      "/api/payments/webhooks/stripe",
                                      //For CI/CD Health Checks
